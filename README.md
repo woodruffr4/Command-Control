3294507e50edbffc8c4cd74715c950717b6c5660
Ricky Woodruff <woodruffr4@gmail.com>
2022-10-07 15:47:51 -0500

    remove cors

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ CS378 Ethical Hacking - Command and Control
 
 ## How to run attacker-server
 
--   Install `flask` and `flask_cors`
+-   Install `flask`
 -   Run `FLASK_APP=attacker-server.py flask run --host={Your server's IP}`
--- attacker-server.py
+++ attacker-server.py
@@ -2,12 +2,9 @@
 # Provides an endpoint which will return a list of commands to run on the target machine
 
 import flask
-from flask_cors import CORS, cross_origin
 
 app = flask.Flask(__name__)
-cors = CORS(app)
 
 @app.route('/commands', methods=['GET'])
-@cross_origin()
 def commands():
     return flask.jsonify(['ls', 'pwd', 'id'])
